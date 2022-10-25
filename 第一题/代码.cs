688634e9ce0579e88d4b3db797c144749f4a66a0
NJunBo <109066831+NJunBo@users.noreply.github.com>
2022-10-25 18:55:59 +0800

    Add files via upload

---
+++ 第一题/代码.cs
@@ -0,0 +1,47 @@
+	private GameObject cube;
+	private Transform begin;
+	private Transform end;
+	private bool pingpong = false;
+	private float distance,disBegin,disEnd;
+	//是否往end走
+	private bool isGo = false;
+	// Use this for initialization
+	void Start()
+	{
+		cube = GameObject.Find("Cube");
+		begin = transform.Find("Start");
+		end = transform.Find("End");	
+	}
+    private void Update()
+    {
+		Move(cube, begin.position, end.position, 2f, pingpong);
+	}
+	private void Move(GameObject gameObject, Vector3 begin, Vector3 end, float time, bool pingpong)
+	{
+		distance = (end - begin).magnitude;
+		if (pingpong)
+		{
+			if (gameObject.transform.position.x >= end.x)
+			{
+				gameObject.transform.position = begin;
+			}
+			gameObject.transform.position += new Vector3(distance / time * Time.deltaTime, 0, 0);
+		}
+		else
+		{
+			disEnd = (gameObject.transform.position - end).magnitude;
+			disBegin = (gameObject.transform.position - begin).magnitude;
+			if (disBegin >= 5 || disEnd >= 5)
+			{
+				isGo = !isGo;
+			}
+			if (isGo)
+			{
+				gameObject.transform.position += new Vector3(distance / time * Time.deltaTime, 0, 0);
+			}
+			else
+			{
+				gameObject.transform.position -= new Vector3(distance / time * Time.deltaTime, 0, 0);
+			}
+		}
+	}
---
+++ 第二题/第二题.cs
@@ -0,0 +1,39 @@
+public int[] Demo(TreeNode root)
+	{
+		if (root == null)
+		{
+			return new int[0];
+		}
+		bool isAdd = false;
+		IList<int> res = new List<int>();
+		Queue<TreeNode> queue = new Queue<TreeNode>();
+		Queue<TreeNode> sonQueue = new Queue<TreeNode>();
+		queue.Enqueue(root);
+		while (queue.Count > 0)
+		{
+			TreeNode node = queue.Dequeue();
+            if (!isAdd)
+            {
+				res.Add(node.val);
+				isAdd = true;
+			}
+			if (node.left != null)
+			{
+				sonQueue.Enqueue(node.left);
+			}
+			if (node.right != null)
+			{
+				sonQueue.Enqueue(node.right);
+			}
+			//将子节点队列赋值给父节点队列
+			if(queue.Count == 0)
+            {
+				for (int i = 0; i < sonQueue.Count; i++)
+				{
+					queue.Enqueue(sonQueue.Dequeue());
+				}
+				isAdd = false;
+			}
+		}
+		return res.ToArray();
+	}
